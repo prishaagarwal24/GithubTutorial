e9954cabda6f1b6991a8b9527403b86d8b03885f
prishaagarwal24 <prisha.dev24@gmail.com>
2024-07-12 11:59:04 -0700

    Initial Commit

---
+++ GithubTutorial/GithubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTutorialApp.swift
+//  GithubTutorial
+//
+//  Created by Prisha Agarwal on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
